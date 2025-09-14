a25e941752cb75ea8bc941ac3add7c181e8b434d
Cub1tuS <129857849+Cub1tuS@users.noreply.github.com>
2025-09-14 15:44:15 +0200

    a

--- tp/terraform/network.tf
+++ tp/terraform/network.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_security_group" "main" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "104.28.30.16"
+    source_address_prefix      = "172.226.148.60"
     destination_address_prefix = "*"
   }
 }
